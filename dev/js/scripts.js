8ad254ff7b185df130dcd3b339a3ca14642c5c70
blairda <bda34@drexel.edu>
2021-04-11 22:23:27 -0400

    Beginning TL anim

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,16 @@
 import { gsap } from "gsap";
 
-gsap.to("#red-circle", { duration: 4, x: 200 });
+// gsap.to("#shape", { duration: 1, x: 800 });
 
-gsap.from("#blue-square", { duration: 4, scale: 2, alpha: 0, delay: 2, repeat: -1, yoyo: true });
+// gsap.from("#blue-square", { duration: 4, scale: 2, alpha: 0, delay: 2, repeat: -1, yoyo: true });
+
+let shapeTL = gsap.timeline();
+
+// shapeTL.from("#shape", {duration: 1, })
+shapeTL.to("#shape", { duration: 1, y: 600 })
+    .to("#shape", { duration: .25, scaleX: 2 })
+    .to("#shape", { duration: .25, scaleX: 1 })
+    .to("#shape", { duration: .25, scaleY: 2, transformOrigin: "center bottom" })
+    .to("#shape", { duration: .25, scaleY: 1 })
+    .to("#shape", {duration: 2, ease: "elastic.inOut(1, 0.5)", y: -1})
+    .to("#shape", { duration: 1, x: 600, scale: 0.5 });

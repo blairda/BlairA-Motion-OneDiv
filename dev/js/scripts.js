90fd0262dc9219ca1c20aa6dc89944b21ebd1bbb
blairda <bda34@drexel.edu>
2021-04-11 23:56:24 -0400

    Middle animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,10 +7,16 @@ import { gsap } from "gsap";
 let shapeTL = gsap.timeline();
 
 // shapeTL.from("#shape", {duration: 1, })
-shapeTL.to("#shape", { duration: 1, y: 600 })
-    .to("#shape", { duration: .25, scaleX: 2 })
-    .to("#shape", { duration: .25, scaleX: 1 })
-    .to("#shape", { duration: .25, scaleY: 2, transformOrigin: "center bottom" })
-    .to("#shape", { duration: .25, scaleY: 1 })
+shapeTL.to("#shape", { duration: 1.5, y: 550, rotate: 360 })
+    .to("#shape", { duration: .25, scaleX: 2.5, scaleY: 0.5 })
+    .to("#shape", { duration: .2, scaleX: 1, scaleY: 1 })
+    .to("#shape", { duration: .25, scaleX: 0.5, scaleY: 2, transformOrigin: "center bottom" })
+    .to("#shape", { duration: .5, scaleX: 1, scaleY: 1 })
     .to("#shape", {duration: 2, ease: "elastic.inOut(1, 0.5)", y: -1})
-    .to("#shape", { duration: 1, x: 600, scale: 0.5 });
+    .to("#shape", { duration: .75, x: 700, scale: 0.5, rotate: 180 })
+    .to("#shape", { duration: .5, skewY: 45, scale: 1 })
+    .to("#shape", { duration: .5, skewY: 0 })
+    .to("#shape", { duration: .25, alpha: 0.5, repeat: 3, yoyo: true, rotateY: 90 })
+    .to("#shape", {duration: 1.75, ease: "power4.out", y: 300 })
+    .to("#shape", { duration: 1, ease: "circ.out", y: 500, x: -15 })
+    ;
